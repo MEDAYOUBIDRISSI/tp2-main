ef510c7f1f2dae6234a31fe96cb9ea786893ff95
med ayoub <medayoub.idrissi@gmail.com>
2021-05-23 20:12:38 +0100

    tp2-main

---
+++ tp2-main/data/Countries.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+
+let countries = [
+    Country(isoCode: "be", name: "Belgium",continent: "Europe"),
+    Country(isoCode: "de", name: "Danemark",continent: "Europe"),
+    Country(isoCode: "fr", name: "France",continent: "Europe"),
+    Country(isoCode: "mr", name: "Morocco",continent: "Afrique"),
+    Country(isoCode: "gr", name: "Germanie",continent: "Europe"),
+    Country(isoCode: "it", name: "Italia",continent: "Europe"),
+]
---
+++ tp2-main/models/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  tp2-main
+//
+//
+
+import Foundation
+
+struct Country {
+    var isoCode: String
+    var name: String
+    var continent: String
+}
